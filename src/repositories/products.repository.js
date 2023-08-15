6ff404c1b3f4fa8306475bba8ac163ae42da7594
ffukamizu <f.fukamizu@live.com>
2023-08-15 16:33:08 -0300

    fix

--- src/repositories/products.repository.js
+++ src/repositories/products.repository.js
@@ -1,4 +1,4 @@
-import { db } from '../database/database.connection';
+import { db } from '../database/database.connection.js';
 
 async function selectProducts() {
     return db.query(`SELECT * FROM products;`);
