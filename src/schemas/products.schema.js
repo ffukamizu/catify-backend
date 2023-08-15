98cdfc7a0cdcbf85f441de87e80922c6d3d8de8c
ffukamizu <f.fukamizu@live.com>
2023-08-15 17:58:09 -0300

    fix

--- src/schemas/products.schema.js
+++ src/schemas/products.schema.js
@@ -3,7 +3,7 @@ import Joi from 'joi';
 const schemaProducts = Joi.object({
     name: Joi.string().required(),
     description: Joi.string().required(),
-    photo: Joi.required(),
+    photo: Joi.string().required(),
 });
 
 export default schemaProducts;
