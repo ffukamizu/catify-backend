db61aa9b8c5fa2449d90e5a0ca3f81cbfdabc450
ffukamizu <f.fukamizu@live.com>
2023-08-16 16:01:27 -0300

    fix

--- src/app.js
+++ src/app.js
@@ -9,14 +9,7 @@ const PORT = process.env.SERVER_PORT || 5000;
 
 const app = express();
 
-const corsOptions = {
-    origin: 'https://catify-front.vercel.app', 
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true, 
-    optionsSuccessStatus: 204,
-};
-
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
 app.use(router);
 
